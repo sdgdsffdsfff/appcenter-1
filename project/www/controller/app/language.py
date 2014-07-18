b22f6d2cdf0712d1f6ef69d6edc8bac7f72947f2
kq <qijianbiao@appvv.com>
2014-07-18 14:47:24 +0800

    Fix language

--- project/www/controller/app/language.py
+++ project/www/controller/app/language.py
@@ -4,4 +4,4 @@ class LanguageController(ControllerBase):
 
     def get(self):
         languages = mongo_db.client_support_language.find({})
-        return [{"code": lang["code"], "name": lang["name"]} for lang in languages]
+        return [{"code": lang["code"], "name": lang["name"], "nameoflanguage": lang["nameoflanguage"]} for lang in languages]
