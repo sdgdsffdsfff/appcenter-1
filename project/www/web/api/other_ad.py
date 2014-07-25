72e35436e46d5d34e9d9825c090d94e997e6b51a
kq <qijianbiao@appvv.com>
2014-07-25 18:18:50 +0800

    Fix

--- project/www/web/api/other_ad.py
+++ project/www/web/api/other_ad.py
@@ -11,6 +11,12 @@ class View(FlaskView):
 class InfoView(View):
     @route('/info', endpoint='api_other_ad_info')
     def get(self):
-        other_add_controller = OtherAdController(ip=request.remote_addr)
+        device = request.args.get("device", "1")
+        language = request.args.get("language", "en")
+        jb = request.args.get("jb", "0")
+        device_s = "iphone" if device == "1"  else "ipad"
+        jb_s = "jb" if jb == "1" else "unjb"
+        cs = jb_s + device_s
+        other_add_controller = OtherAdController(language=language, ip=request.remote_addr, cs=cs)
         other_add = other_add_controller.get()
         return self._view.render(1000, list(other_add))
