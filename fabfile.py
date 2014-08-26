30cc0f7433ccacffd4a9ca8d54083bbc2fee6e9f
kq <qijianbiao@appvv.com>
2014-08-26 15:45:51 +0800

    Fix

--- fabfile.py
+++ fabfile.py
@@ -1,15 +1,15 @@
 from fabric.api import local
 from fabric.api import *
 
-env.roledefs['test_web_server'] = ['ops@192.168.2.240:22']
+env.roledefs['test_web_server'] = ['ops@192.168.2.241:22']
 
 env.passwords = {
-    'ops@192.168.2.240:22': '2wsx1qaz',
+    'ops@192.168.2.241:22': '2wsx1qaz',
 }
 
 @roles("test_web_server")
 def deploy_test():
-    project_dir = '/data0/wwwroot/appcenter/project'
+    project_dir = '/home/ops/appcenter/project'
     with cd(project_dir):
         run('git pull')
-        run("supervisorctl restart appcenter")
+        run("sudo supervisorctl restart appcenter")
