f63a744edaa4802a759b8063fc876ec17b59866f
kq <qijianbiao@appvv.com>
2014-09-27 21:43:05 +0800

    Fix

--- project/www/web/admin/app.py
+++ project/www/web/admin/app.py
@@ -626,7 +626,7 @@ class ScreenshotView(View):
                 tmp_file = os.path.join(settings['tmp_dir'], name)
                 file.save(tmp_file)
                 sha1 = sha1_of_file(tmp_file)
-                pic_path = hash_to_path(sha1) + '.' +ext
+                pic_path = hash_to_path(sha1) + ".114x114-75" + '.' +ext
                 pic_path = os.path.join(settings['pic_upload_dir'], pic_path)
                 dir_path = os.path.dirname(pic_path)
                 if not os.path.isdir(dir_path):
