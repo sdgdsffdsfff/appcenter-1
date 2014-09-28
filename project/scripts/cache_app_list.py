ea198f504deeb79ca67cb4ed88c697def8afc9ae
kq <qijianbiao@appvv.com>
2014-09-28 11:44:19 +0800

    Fix

--- project/scripts/cache_app_list.py
+++ project/scripts/cache_app_list.py
@@ -15,23 +15,17 @@ mongo_db = mongo.get_database('appcenter')
 
 
 def cache_app_list(genre_id):
-    try:
-        print "Start cache genre: %s app list" % genre_id
+    print "Start cache genre: %s app list" % genre_id
 
-        #更新推荐缓存前100页
-        app = AppController()
-        print 'Cache sort by sort'
-        app.set_apps_cache(genre_id, ('sort', -1)) #推荐
-        print 'Cache sort by downloadCount'
-        app.set_apps_cache(genre_id, ('downloadCount', -1)) #最热
-        print 'Cache sort by _id'
-        app.set_apps_cache(genre_id, ('_id', -1)) #最新
-
-        print "Cache %s OK" % genre_id
-    except Exception, ex:
-        import traceback
-        traceback.print_exc()
-        print "Cache %s ERROR: %s" % (genre_id, ex)
+    #更新推荐缓存前100页
+    app = AppController()
+    print 'Cache sort by sort'
+    app.set_apps_cache(genre_id, ('sort', -1)) #推荐
+    print 'Cache sort by downloadCount'
+    app.set_apps_cache(genre_id, ('downloadCount', -1)) #最热
+    print 'Cache sort by _id'
+    app.set_apps_cache(genre_id, ('_id', -1)) #最新
+    print "Cache %s OK" % genre_id
 
 
 def cache_app_list_run(genreID=None):
