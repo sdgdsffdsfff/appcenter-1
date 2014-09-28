d93579642b3d0ebaba6912ef867dbcc6f21f16f2
kq <qijianbiao@appvv.com>
2014-09-28 10:36:19 +0800

    Fix

---
+++ project/scripts/update_app_base_sign.py
@@ -0,0 +1,20 @@
+from pymongo import MongoClient
+from datetime import datetime
+
+TO_MONGO_SEVRER_URL = "mongodb://appcenter:tuj62_Iga1e4_a@54.183.152.170:37017,54.72.191.195:37017,61.155.215.40:37017/appcenter"
+to_client = MongoClient(TO_MONGO_SEVRER_URL)
+to_db = to_client["appcenter"]
+
+def transfer():
+    downloads = to_db.AppDownload.find({})
+    for index, download in enumerate(downloads):
+        bundle_id = download.get("bundleId", None)
+        if not bundle_id: continue
+        sign = download.get("sign", 0)
+        data = {"review": 1}
+        if sign == 1: data["sign"] = sign
+        print index
+        app_base = to_db.AppBase.update({"bundleId": bundle_id}, {"$set": data})
+
+if __name__ == "__main__":
+    transfer()
