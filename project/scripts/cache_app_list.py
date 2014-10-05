96add02b4d0629289c612477f85ba1b83d4e2392
kq <qijianbiao@appvv.com>
2014-10-05 18:04:13 +0800

    Fix

--- project/scripts/cache_app_list.py
+++ project/scripts/cache_app_list.py
@@ -9,9 +9,6 @@ from common.ng_daemon import NGDaemon
 from conf.settings import settings
 from common.ng_mongo import NGMongoConnect
 
-import gevent.monkey
-gevent.monkey.patch_socket()
-import gevent
 
 mongo = NGMongoConnect(settings['mongodb']['host'], replica_set=settings["mongodb"].get("replica_set", None))
 mongo_db = mongo.get_database('appcenter')
@@ -27,10 +24,11 @@ def cache_app_list(genre_id):
 
 
 def asynchronous_cache_genres():
-    threads = []
-    for index, genre in enumerate(mongo_db.app_genre.find()):
-        threads.append(gevent.spawn(cache_app_list, str(genre['genreId'])))
-    gevent.joinall(threads)
+    pass
+    # threads = []
+    # for index, genre in enumerate(mongo_db.app_genre.find()):
+    #     threads.append(gevent.spawn(cache_app_list, str(genre['genreId'])))
+    # gevent.joinall(threads)
 
 def synchronous_cache_genres():
     for index, genre in enumerate(mongo_db.app_genre.find()):
