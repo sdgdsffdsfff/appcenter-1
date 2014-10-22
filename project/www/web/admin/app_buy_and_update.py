4ebd803a493577a5e4a7316d245e083efbcb56b7
Chad Ying <anjiannian@gmail.com>
2014-10-22 14:15:28 +0800

    modify app buy and update process

--- project/www/web/admin/app_buy_and_update.py
+++ project/www/web/admin/app_buy_and_update.py
@@ -1,5 +1,5 @@
 #encoding=UTF8
-import main, hashlib
+import main, hashlib, math
 from flask.ext.login import UserMixin
 from __header__ import AdminView, FlaskView
 from __header__ import DB, route
@@ -7,6 +7,8 @@ from flask.ext.login import current_user
 from functools import wraps
 from flask import request, abort
 from bson.objectid import ObjectId
+from datetime import datetime
+from random import randint
 
 class View(FlaskView):
     route_base = '/app-buy-manager'
@@ -16,22 +18,64 @@ class View(FlaskView):
 class ListView(View):
     @route('/list', endpoint='app_buy')
     def get(self):
-        return self._view.render('app_buy_manage')
+        page = int(request.args.get('page', 1))
+        page_size = request.args.get('page_size', 10)
+        res = DB.app_process.find({'apple_account': {'$exists': False},
+                                   'status': {'$ne': 'finished'},
+                                   'editor': current_user.username}). \
+            sort('status').skip((page -1)*page_size).limit(page_size)
+        total_page = int(math.ceil(res.count() / float(page_size)))
+        offset = (page - 1) * page_size
+        prev_page = (page - 1) if page - 1 > 0 else 1
+        next_page = (page + 1) if page + 1 < total_page else total_page
+        page_info = {"count": res.count(), "page": page, "total_page": total_page,
+                     "prev_page": prev_page, 'next_page': next_page}
+        to_buy_count = DB.app_process.find({'apple_account': {'$exists': False}, 'status': {'$ne': 'finished'}}).count()
+        to_update_count = DB.app_process.find({'apple_account': {'$exists': True}, 'status': {'$ne': 'finished'}}).count()
+        to_process_count = DB.app_process.find({'status': {'$ne': 'finished'}}).count()
+        return self._view.render('app_buy_manage', results=res,
+                                 to_buy_count=to_buy_count,
+                                 to_update_count=to_update_count,
+                                 to_process_count=to_process_count,
+                                 page_info=page_info)
 
 class UpdateListView(View):
     @route('/update_list', endpoint='app_update')
     def get(self):
-        return self._view.render('app_update_manage')
+        page = int(request.args.get('page', 1))
+        page_size = request.args.get('page_size', 10)
+        res = DB.app_process.find({'apple_account': {'$exists': True},
+                                   'status': {'$ne': 'finished'},
+                                   'editor': current_user.username}). \
+            sort('status').skip((page -1)*page_size).limit(page_size)
+        total_page = int(math.ceil(res.count() / float(page_size)))
+        offset = (page - 1) * page_size
+        prev_page = (page - 1) if page - 1 > 0 else 1
+        next_page = (page + 1) if page + 1 < total_page else total_page
+        page_info = {"count": res.count(), "page": page, "total_page": total_page,
+                     "prev_page": prev_page, 'next_page': next_page}
+        return self._view.render('app_update_manage', results=res, page_info=page_info)
 
 class AllListView(View):
     @route('/all_list', endpoint='app_all')
     def get(self):
-        return self._view.render('app_all_manage')
+        page = int(request.args.get('page', 1))
+        page_size = request.args.get('page_size', 10)
+        res = DB.app_process_log.find({'editor': current_user.username}). \
+            sort('status').skip((page -1)*page_size).limit(page_size)
+        total_page = int(math.ceil(res.count() / float(page_size)))
+        offset = (page - 1) * page_size
+        prev_page = (page - 1) if page - 1 > 0 else 1
+        next_page = (page + 1) if page + 1 < total_page else total_page
+        page_info = {"count": res.count(), "page": page, "total_page": total_page,
+                     "prev_page": prev_page, 'next_page': next_page}
+        return self._view.render('app_all_manage', results=res, page_info=page_info)
+
 
 class GetTaskView(View):
     @route('/get-task', endpoint='get_buy_task')
     def get(self):
-        track_id = "623592465"
+        track_id = str(randint(100000000, 999999999))
         track_name = "Heads Up!"
         version = "2.3.5"
         price = 19
@@ -45,6 +89,42 @@ class GetTaskView(View):
             "link_url": link_url,
             "currency": currency,
             "status": "new",
+            "editor": current_user.username,
+            "recieve_time": datetime.utcnow()
         }
-        DB.new_app_task.insert(new_app_task)
-        return self._view.ajax_response(200, new_app_task)
+        DB.app_process.insert(new_app_task)
+        new_app_task['recieve_time'] = new_app_task['recieve_time'].strftime('%Y-%m-%d')
+        return self._view.ajax_response('success', new_app_task)
+
+
+class BuyAppView(View):
+    @route('/buy', methods=['POST'], endpoint='buy_app')
+    def post(self):
+        try:
+            track_id = request.form.get('track_id', '')
+            res = DB.app_process.update({'track_id': track_id},
+                                        {'$set': {'status': 'processing'}})
+            status, message = 'success', res
+            data = DB.app_process.find_one({'track_id': track_id}, {'_id': 0})
+            data['buy_time'] = datetime.utcnow()
+            DB.app_process_log.insert(data)
+        except Exception, ex:
+            status, message = 'error', str(ex.message)
+
+        return self._view.ajax_response(status, message)
+
+
+class UpdateAppView(View):
+    @route('/update', methods=['POST'], endpoint='update_app')
+    def post(self):
+        try:
+            track_id = request.form.get('track_id', '')
+            res = DB.app_process.update({'track_id': track_id},
+                                        {'$set': {'status': 'processing'}})
+            status, message = 'success', res
+            data = DB.app_process.find_one({'track_id': track_id}, {'_id': 0})
+            data['update_time'] = datetime.utcnow()
+            DB.app_process_log.insert(data)
+        except Exception, ex:
+            status, message = 'error', str(ex.message)
+        return self._view.ajax_response(status, message)
