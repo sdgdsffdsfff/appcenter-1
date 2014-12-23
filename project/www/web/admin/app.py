bac60dc3fa054e7addd1e7cd31ce0361c3af3276
kq <qijianbiao@appvv.com>
2014-12-23 18:33:02 +0800

    Fix

--- project/www/web/admin/app.py
+++ project/www/web/admin/app.py
@@ -673,10 +673,10 @@ class ScreenshotView(View):
                     DB.AppBase.update({'bundleId': bundle_id}, {'$addToSet': data})
                 else:
                     try:
-                        DB.AppBase_CN.update({'bundleId': bundle_id}, {'$addToSet': data}, upsert=True)
+                        DB.AppBase_CN.update({'bundleId': bundle_id}, {'$addToSet': data})
                     except Exception:
                         data = data.update({'bundleId': bundle_id})
-                        DB.AppBase_CN.insert(data)
+                        #DB.AppBase_CN.insert(data)
                 status, message = 'success', u'上传成功'
             else:
                 raise Exception(u'必须是jpg,png文件')
