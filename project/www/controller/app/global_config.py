61b976b554b0adb26d589ef1b01bf6a6571d78de
kq <qijianbiao@appvv.com>
2015-02-10 15:01:28 +0800

    Fix: global

--- project/www/controller/app/global_config.py
+++ project/www/controller/app/global_config.py
@@ -4,5 +4,5 @@ class GlobalConfigController(ControllerBase):
 
     def get(self):
         global_config = mongo_db.global_config.find({})
-        if global_config: return dict()
+        if not global_config: return dict()
         else: return global_config[0]["data"]
