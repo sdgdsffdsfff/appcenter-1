697c19ab044dc0b275c0c7613af220a093b48c07
kq <qijianbiao@appvv.com>
2015-03-02 15:19:22 +0800

    Fix:Remove scripts

--- project/manager.py
+++ project/manager.py
@@ -3,38 +3,38 @@ from flask.ext.script import Manager
 
 from main import app, register_view
 
-from scripts.update_app_info import *
-from scripts.indexer import build_search_index_run
-from scripts.cache_app import cache_app_run
-from scripts.cache_app_list import cache_app_list_run
-from scripts.compare_update import track_need_to_update
-from scripts.sync_app_CN import fetch_appbase
+# from scripts.update_app_info import *
+# from scripts.indexer import build_search_index_run
+# from scripts.cache_app import cache_app_run
+# from scripts.cache_app_list import cache_app_list_run
+# from scripts.compare_update import track_need_to_update
+# from scripts.sync_app_CN import fetch_appbase
 # from scripts.new_transfer_topic_to_db import update_app_topic
 
 manager = Manager(app)
 register_view()
 
 
-@manager.command
-def build_search_index(): build_search_index_run()
+# @manager.command
+# def build_search_index(): build_search_index_run()
 
-@manager.option('-I', '--ID', help='App mongo obejct id ')
-def cache_app(ID): cache_app_run(ID)
+# @manager.option('-I', '--ID', help='App mongo obejct id ')
+# def cache_app(ID): cache_app_run(ID)
 
-@manager.option('-G', '--genreID', help='App genre id ')
-def cache_app_list(genreID):
-    cache_app_list_run(genreID)
+# @manager.option('-G', '--genreID', help='App genre id ')
+# def cache_app_list(genreID):
+#     cache_app_list_run(genreID)
 
-@manager.command
-def update_app(): recursive_update_app_info()
+# @manager.command
+# def update_app(): recursive_update_app_info()
 
-@manager.command
-def compare_update():
-	track_need_to_update()
+# @manager.command
+# def compare_update():
+# 	track_need_to_update()
 
-@manager.command
-def sync_app_info():
-	fetch_appbase()
+# @manager.command
+# def sync_app_info():
+# 	fetch_appbase()
 
 
 # @manager.command
